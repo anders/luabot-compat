35648a18ba500effd341152a2519cd0f37ae7eb0
Anders Bergh <anders1@gmail.com>
2014-05-07 14:02:35 +0200

    Initial commit.

---
+++ luabot-compat.lua
@@ -0,0 +1,39 @@
+etc = {
+  cmdchar = "'"
+}
+plugin = {}
+tests = {}
+
+nick = 'anders'
+bot = 'luabot'
+boturl = 'http://localhost/'
+cmdchar = '\\'
+
+nicklist = function()
+  return {'anders', 'byte[]', 'wm4'}
+end
+
+names = function(sep)
+  return table.concat(nicklist(), sep or ' ')
+end
+
+rnick = function()
+  local t = nicklist()
+  return t[math.random(1, #t)]
+end
+
+getname = function()
+  return 'anders'
+end
+
+account = 2
+
+local history = {
+  {'this is a message', 'anders', os.time()},
+  {'i maek a typo', 'anders', os.time() - 12},
+  {'good one fr8', 'byte[]', os.time() - 24}
+}
+
+_getHistory = function(n)
+  return unpack(history[n])
+end
